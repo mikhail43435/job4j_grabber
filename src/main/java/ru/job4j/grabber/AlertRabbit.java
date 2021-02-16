7b7064d1cd27e06d53d181b98e7f20df76f6eaf0
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-02-16 15:34:31 +0300

    Quartz [#391154]

---
+++ src/main/java/ru/job4j/grabber/AlertRabbit.java
@@ -0,0 +1,47 @@
+package ru.job4j.grabber;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        int interval;
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(in);
+            interval = Integer.parseInt(config.getProperty("rabbit.interval"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(interval)
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
--- src/main/java/ru/job4j/grabber/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.grabber;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
