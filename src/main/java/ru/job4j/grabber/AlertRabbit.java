89067b7b1452f6229d4abc5462ac1f7d8621b4ba
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-02-17 16:54:30 +0300

    Quartz [#391154]

--- src/main/java/ru/job4j/grabber/AlertRabbit.java
+++ src/main/java/ru/job4j/grabber/AlertRabbit.java
@@ -12,15 +12,7 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
     public static void main(String[] args) {
-        int interval = 0;
-        try (InputStream inputStream =
-                     AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-            Properties config = new Properties();
-            config.load(inputStream);
-            interval = Integer.parseInt(config.getProperty("rabbit.interval"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        int interval = getIntervalTime();
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
@@ -38,6 +30,28 @@ public class AlertRabbit {
         }
     }
 
+    /**
+     * Метод возвращает время интервало в секундах
+     * полученно из файла свойств rabbit.properties.
+     * @return int время интервала в секундах
+     */
+    private static int getIntervalTime() {
+        int interval = 1;
+        try (InputStream inputStream =
+                     AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            Properties config = new Properties();
+            config.load(inputStream);
+            interval = Integer.parseInt(config.getProperty("rabbit.interval"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return interval;
+    }
+
+    /**
+     * Объект с типом org.quartz.Job
+     * Внутри класса содержаться требуесые действия.
+     */
     public static class Rabbit implements Job {
         @Override
         public void execute(JobExecutionContext context) {
