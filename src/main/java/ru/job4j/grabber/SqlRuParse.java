2fec75a66528a543a8456c7ad7fdc526b536cdb9
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-02-18 19:03:34 +0300

    Загрузка деталей поста. [#391162]

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -5,13 +5,14 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 import static java.util.Objects.nonNull;
 
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < 2; i++) {
             System.out.println("Printing page " + i + "...");
             System.out.println("-----------------");
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
@@ -19,12 +20,20 @@ public class SqlRuParse {
             for (Element elementTrTag : el) {
                 Elements element1 = elementTrTag.getElementsByClass("postslisttopic");
                 if (nonNull(element1.first()) && nonNull(elementTrTag.child(5))) {
+                    String link = elementTrTag.child(1).getElementsByTag("a").attr("href");
                     System.out.println("Topic label: " + element1.first().text());
-                    System.out.println("Post date: " + SlqRuDateParser.parseDate(elementTrTag.child(5).text()));
-                    System.out.println("-----------------");
+                    System.out.println("Date: " + SlqRuDateParser.parseDate(elementTrTag.child(5).text()));
+                    System.out.println("URL: " + link);
+                    System.out.println(getBody(link));
+                    System.out.println("-----------------------------------------------------");
                 }
             }
         }
+    }
 
+    private static String getBody(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        Elements el = doc.getElementsByClass("msgBody");
+        return el.get(1).text();
     }
 }
