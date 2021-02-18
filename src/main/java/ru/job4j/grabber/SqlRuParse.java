c4f0cdd3cf78964c5cf19869a29d962b9b276a46
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-02-18 14:50:03 +0300

    Парсинг https://www.sql.ru/forum/job-offers/3 [#391160]

--- src/main/java/ru/job4j/grabber/SqlRuParse.java
+++ src/main/java/ru/job4j/grabber/SqlRuParse.java
@@ -10,15 +10,21 @@ import static java.util.Objects.nonNull;
 public class SqlRuParse {
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements el = doc.getElementsByTag("tr");
-        for (Element elementTrTag : el) {
-            Elements element1 = elementTrTag.getElementsByClass("postslisttopic");
-            if (nonNull(element1.first()) && nonNull(elementTrTag.child(5))) {
-                System.out.println("Topic label: " + element1.first().text());
-                System.out.println("Post date: " + SlqRuDateParser.parseDate(elementTrTag.child(5).text()));
-                System.out.println("-----------------");
+
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Printing page " + i + "...");
+            System.out.println("-----------------");
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
+            Elements el = doc.getElementsByTag("tr");
+            for (Element elementTrTag : el) {
+                Elements element1 = elementTrTag.getElementsByClass("postslisttopic");
+                if (nonNull(element1.first()) && nonNull(elementTrTag.child(5))) {
+                    System.out.println("Topic label: " + element1.first().text());
+                    System.out.println("Post date: " + SlqRuDateParser.parseDate(elementTrTag.child(5).text()));
+                    System.out.println("-----------------");
+                }
             }
         }
+
     }
 }
