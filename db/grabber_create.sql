22ec44f68864591b6e7468169ff9b4fc4067c033
mikhail43435 <68198830+mikhail43435@users.noreply.github.com>
2021-02-19 16:35:47 +0300

    Схема таблицы Post [#391167]

---
+++ db/grabber_create.sql
@@ -0,0 +1,9 @@
+create table if not exists
+post(
+    id serial primary key,
+    name varchar(200),
+    text varchar(1000),
+    link varchar(255) NOT NULL,
+    created_date timestamp,
+    CONSTRAINT link_unique UNIQUE (link)
+);
---
+++ db/grabber_insert.sql
@@ -0,0 +1,5 @@
+insert into post
+(name, text, link, created_date)
+values
+('test', 'test text', 'test link', '01.01.2021')
+;
